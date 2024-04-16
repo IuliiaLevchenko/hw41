820356916631c842bf8409dad3a43589e9796490
Iuliia_Levchenko <levchenkoyuliya@gmail.com>
2024-04-16 16:57:49 +0200

    hw41

--- generateYears1.js
+++ generateYears1.js
@@ -1,13 +1,23 @@
+//Функція для підрахування останньоі цифри і підстановки правильного слова рік, років, роки
 function generateYears(input) {
   let numInput = parseInt(input); // Конвертація введення у число
 
   let suffix;
+  // знаходимо решту від діленя на 10, якщо дорівнює 1, але не 11 і не 111
   if (numInput % 10 === 1 && numInput % 100 !== 11) {
-    suffix = 'рік';
-  } else if (numInput % 10 >= 2 && numInput % 10 <= 4 && !(numInput % 100 >= 12 && numInput % 100 <= 14)) {
-    suffix = 'роки';
+    // add word "рік"
+    suffix = "рік";
+    // знаходимо решту від діленя на 10, якщо дорівнює від 2 до 4, але не 12, 13, 14
+  } else if (
+    numInput % 10 >= 2 &&
+    numInput % 10 <= 4 &&
+    !(numInput % 100 >= 12 && numInput % 100 <= 14)
+  ) {
+    // add word "роки"
+    suffix = "роки";
   } else {
-    suffix = 'років';
+    // якщо попередні дві умови не виконуються, то додаєм 'років'
+    suffix = "років";
   }
 
   document.body.innerHTML = `<p>${numInput} ${suffix}</p>`; // Вивід результату на сторінці
